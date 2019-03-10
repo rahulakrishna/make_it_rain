01837fd408a903c9005900d51c7f98df6d550d18
Rahul Krishna <rkbalu524@gmail.com>
2019-03-10 12:29:53 +0530

    ✨ First implementation of state!😄

--- lib/components/make_it_rain.dart
+++ lib/components/make_it_rain.dart
@@ -9,6 +9,14 @@ class MakeItRain extends StatefulWidget {
 }
 
 class MakeItRainState extends State<MakeItRain> {
+  int _moneys = 0;
+
+  void _rainMoney() {
+    setState(() {
+      _moneys = _moneys + 1244;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -25,7 +33,24 @@ class MakeItRainState extends State<MakeItRain> {
                 color: Colors.black,
                 fontWeight: FontWeight.w800,
                 fontSize: 24.5),
-          ))
+          )),
+          new Expanded(
+              child: new Center(
+                  child: new Text(
+            '₹$_moneys',
+            style: new TextStyle(
+                color: Colors.green, fontSize: 46, fontWeight: FontWeight.w800),
+          ))),
+          new Expanded(
+              child: new Center(
+                  child: new RaisedButton(
+                      color: Colors.green,
+                      textColor: Colors.white,
+                      onPressed: _rainMoney,
+                      child: new Text(
+                        'Rain!',
+                        style: new TextStyle(fontSize: 19),
+                      ))))
         ],
       )),
     );
